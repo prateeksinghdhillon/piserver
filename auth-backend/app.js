843609fb480824059f638889ed21f76611cf1cb1
prateeksinghdhillon <prateekdhillon58@gmail.com>
2024-09-28 15:04:08 +0530

    console removed

--- auth-backend/app.js
+++ auth-backend/app.js
@@ -8,7 +8,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-console.log(process.env.MONGO_URI)
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
